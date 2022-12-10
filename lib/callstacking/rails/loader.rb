e307b2e49b0772bc2abbdc5a7bcb5303fe21e2ab
Jim Jones <jim.jones1@gmail.com>
2022-12-10 11:20:22 -0600

    Properly wrapping module singleton methods.

--- lib/callstacking/rails/loader.rb
+++ lib/callstacking/rails/loader.rb
@@ -3,21 +3,45 @@ require "rails"
 module Callstacking
   module Rails
     class Loader
-      attr_accessor :loader, :root
+      attr_accessor :loader, :root, :once
       def initialize
         @loader = ::Rails.autoloaders.main
+        @once   = ::Rails.autoloaders.once
         @root =  Regexp.new(::Rails.root.to_s)
       end
 
       def on_load
-        loader.on_load do |cpath, value, abspath|
-          if abspath =~ root
-            puts "cpath = #{cpath} - #{abspath}"
-            klass = cpath.constantize
+        trace = TracePoint.new(:end) do |tp|
+          klass = tp.self
+          path  = tp.path
+
+          if path =~ root
             klass.include(Callstacking::Rails::Span)
             klass.init(klass)
           end
         end
+        trace.enable
+
+        # ::Rails.autoloaders.log!
+        # loader.on_load do |cpath, value, abspath|
+        #   puts "cpath = #{cpath} - #{abspath}"
+        #
+        #   if abspath =~ root
+        #     klass = cpath.constantize
+        #     klass.include(Callstacking::Rails::Span)
+        #     klass.init(klass)
+        #   end
+        # end
+        
+        # @once.on_load do |cpath, value, abspath|
+        #   puts "cpath2 = #{cpath} - #{abspath}"
+        #
+        #   if abspath =~ root
+        #     klass = cpath.constantize
+        #     klass.include(Callstacking::Rails::Span)
+        #     klass.init(klass)
+        #   end
+        # end
       end
     end
   end
--- lib/callstacking/rails/span.rb
+++ lib/callstacking/rails/span.rb
@@ -11,8 +11,15 @@ module Callstacking
       module ClassMethods
         mattr_accessor :spans
         def add_method(method_name, klass)
-          tmp_module = find_or_initialize_module
-          return if tmp_module.instance_methods(false).include?(method_name)
+          tmp_module = find_or_initialize_module(klass)
+          return if tmp_module.instance_methods.include?(method_name) ||
+                      tmp_module.singleton_methods.include?(method_name)
+
+          m = klass.instance_method(method_name).source_location.first rescue nil
+          m||= klass.method(method_name).source_location.first rescue nil
+
+          # Only application level calls
+          return unless m =~ /#{::Rails.root.to_s}/
 
           tmp_module.define_method(method_name) do |*args, &block|
             klass = self.class.to_s
@@ -26,28 +33,34 @@ module Callstacking
             @@spans.call_entry(klass, method_name, p || path, l || line_no)
             return_val = super(*args, &block)
             @@spans.call_return(klass, method_name, p || path, l || line_no, return_val)
-            return_val
+            
+            return_val.inspect
           end
         end
 
-        def find_or_initialize_module
+        def find_or_initialize_module(klass)
           module_name = "#{name.gsub('::', '')}Span"
           module_index = ancestors.map(&:to_s).index(module_name)
 
           unless module_index
-            prepend Object.const_set(module_name, Module.new)
-            return find_or_initialize_module
+            new_module = Object.const_set(module_name, Module.new)
+            
+            prepend new_module
+            klass.singleton_class.prepend new_module if klass.class == Module
+            
+            return find_or_initialize_module(klass)
           end
 
           ancestors[module_index]
         end
 
         def init(k)
-          # No need to init Modules - they'll get included in classes
-          #  and those classes can be wrapped.
-          return if k.class == Module
+          methods = (k.instance_methods +
+                     k.private_instance_methods(false) +
+                     k.protected_instance_methods(false) +
+                     k.methods +
+                     k.singleton_methods).uniq
 
-          methods = (k.instance_methods(false) + k.private_instance_methods(false) + k.protected_instance_methods(false) + k.methods(false)).uniq
           @filtered||=(Object.instance_methods + Object.private_instance_methods + Object.protected_instance_methods + Object.methods(false)).uniq
 
           relevant = methods - @filtered
