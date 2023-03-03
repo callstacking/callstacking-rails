d76a7edc11bc11659ef60fda3402244b40a4ab71
deyemiobaa <sodiqaderibigbe90@gmail.com>
2023-03-03 11:48:52 +0100

    convert trace call to async

--- lib/callstacking/rails/client/trace.rb
+++ lib/callstacking/rails/client/trace.rb
@@ -1,4 +1,7 @@
 require "callstacking/rails/client/base"
+require "async"
+require "async/http/internet"
+require "json"
 
 module Callstacking
   module Rails
@@ -8,21 +11,27 @@ module Callstacking
         UPDATE_URL = "/api/v1/traces/:id.json"
 
         def create(method_name, klass, action_name, format_name, root_path, url, request_id, headers, params)
-          resp = post(CREATE_URL,
-                      {},
-                      { method_name: method_name,
-                        klass: klass,
-                        action_name: action_name,
-                        format_name: format_name,
-                        root_path: root_path,
-                        url: url,
-                        request_id: request_id,
-                        h: headers.to_h,
-                        p: params.to_h,
-                      })
+          Async do
+            internet = Async::HTTP::Internet.new
+            headers = {}
+            body = JSON.dump({ method_name: method_name,
+                          klass: klass,
+                          action_name: action_name,
+                          format_name: format_name,
+                          root_path: root_path,
+                          url: url,
+                          request_id: request_id,
+                          h: headers.to_h,
+                          p: params.to_h,
+                        })
+            resp = internet.post(CREATE_URL, headers, body)
 
-          if resp.status == 200
-            return resp.body["trace_id"], resp.body["pulse_interval"], resp.body["max_trace_entries"]
+            if resp.status == 200
+              return resp.body["trace_id"], resp.body["pulse_interval"], resp.body["max_trace_entries"]
+            end
+          
+          ensure 
+            internet&.close
           end
 
           nil
