491f5bc9016c5c46f25ab062b5de7ea9b8b91c25
Jim Jones <jim.jones1@gmail.com>
2023-04-16 19:22:26 -0500

    Fixed HUD integration tests.

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 class NavigationTest < ActionDispatch::IntegrationTest
   test "HUD is loaded" do
-    get '/'
+    get '/?debug=1'
     assert_match(/Hello/, @response.body)
     assert_match(/iframe/, @response.body)
     assert_match(/#{Callstacking::Rails::Trace::ICON}/, @response.body)
-    assert_match(/#{Callstacking::Rails::Settings::PRODUCTION_URL}/, @response.body)
+    assert_match(/#{Callstacking::Rails::Settings::PRODUCTION_URL}\/traces/, @response.body)
+  end
+
+  test "HUD is not loaded" do
+    get '/'
+    assert_match(/Hello/, @response.body)
+    assert_no_match(/iframe/, @response.body)
+    assert_no_match(/#{Callstacking::Rails::Trace::ICON}/, @response.body)
+    assert_no_match(/#{Callstacking::Rails::Settings::PRODUCTION_URL}\/traces/, @response.body)
   end
 end
